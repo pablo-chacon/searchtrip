2713d455027b3fb1d7eea325925f27189fd638cc
pablo <ekarlsson66@gmail.com>
2023-10-29 20:06:50 +0100

    Complaint endpoint in TripCointroller

--- src/main/java/com/example/searchtrip/controller/TripController.java
+++ src/main/java/com/example/searchtrip/controller/TripController.java
@@ -105,5 +105,10 @@ public class TripController {
 
         return response.getBody();
     }
+
+    @PostMapping("/complaint")
+    public void addComplaint(@RequestParam String complaint) {
+        System.out.println(complaint);
+    }
 }
 
